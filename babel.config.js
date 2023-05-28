dca08db80d2ee063974ce1afe8bcf4c295714ef2
Nam Thai <nam.thai.dev@outlook.com>
2023-05-28 15:13:43 +0700

    Backbone setup

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      [
-        "babel-preset-expo",
-        {
-          // Use React 17 automatic JSX runtime.
-          jsxRuntime: "automatic",
-        },
-      ],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+      require.resolve("expo-router/babel"),
     ],
-    plugins: ["react-native-reanimated/plugin"],
   };
 };
